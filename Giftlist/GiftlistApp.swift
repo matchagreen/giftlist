fe71dabe2804d19104e47f1d68517df97cb8b042
Alan Perez <alanperezse@gmail.com>
2023-02-09 21:02:52 -0600

    Initial Commit

---
+++ Giftlist/GiftlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftlistApp.swift
+//  Giftlist
+//
+//  Created by Alan Perez on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GiftlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
